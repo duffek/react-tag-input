752714c144bf21604085e4127a000edce528cb89
David Nelson <david@duffekmobile.com>
2020-08-21 14:19:06 -0400

    ?

---
+++ dist/styled/Wrapper.js
@@ -0,0 +1,9 @@
+var _templateObject = _taggedTemplateLiteral(['\n  background: #F1F3F4;\n  box-shadow: 3px 3px 10px rgba(0,0,0, 0.1);\n  font-family: \'Hind\', sans-serif;\n  font-weight: 400;\n  border-radius: 20px;\n  padding: 10px;\n  font-size: large;\n  position: absolute;\n  left: 50%;\n  top: 50%;\n  transform: translate(-50%, -50%);\n  width: 70%;\n'], ['\n  background: #F1F3F4;\n  box-shadow: 3px 3px 10px rgba(0,0,0, 0.1);\n  font-family: \'Hind\', sans-serif;\n  font-weight: 400;\n  border-radius: 20px;\n  padding: 10px;\n  font-size: large;\n  position: absolute;\n  left: 50%;\n  top: 50%;\n  transform: translate(-50%, -50%);\n  width: 70%;\n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+import styled from 'styled-components';
+
+var Wrapper = styled.div(_templateObject);
+
+export default Wrapper;
